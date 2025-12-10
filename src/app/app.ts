0853a774703e8e2b48b811340914d10deb6659b7
laethenin <laetitiahenin@gmail.com>
2025-12-10 12:12:23 +0100

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CharactersList } from './components/characters-list/characters-list';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CharactersList],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('got-portfolio');
+}
